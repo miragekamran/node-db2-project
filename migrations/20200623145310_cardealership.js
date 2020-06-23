39a4bb945bc56bd79571bb3aa8e4950c49bebb59
mirage <mirage.kamran.pkt@gmail.com>
2020-06-23 16:46:54 -0700

    migration file updated

--- migrations/20200623145310_cardealership.js
+++ migrations/20200623145310_cardealership.js
@@ -1,8 +1,14 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+    await knex.schema.createTable("cars", table => {
+        table.increments("id");
+        table.string("vin").notNull().unique();
+        table.integer("make").notNull();
+        table.text("model").notNull().unique();
+        table.float("mileage").notNull();
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists("cars");
 };
